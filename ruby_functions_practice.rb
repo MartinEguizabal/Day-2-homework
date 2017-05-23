a9b40279b9ddffeb6247adf8ebc48a574e69895d
MartyCodeClan <zabbalab@hotmail.com>
2017-05-23 17:16:31 +0100

    2nd and 3rd tests passed

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -1,3 +1,15 @@
 def return_10()
   return 10
 end
+
+def add(number1, number2)
+  return number1 + number2
+end
+
+def subtract(number1, number2)
+  return number1 - number2
+end
+
+def multiply(number1, number2)
+  return number1 * number2
+end
